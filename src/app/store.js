09a944101cf124c6b6da3982268162e28c885925
Paul Noh <paul@Pauls-MacBook-Air.local>
2023-10-22 15:57:47 +0900

    Using Redux Data

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 
 import postsReducer from '../features/posts/postsSlice';
+import usersReducer from '../features/users/usersSlice';
 
 const rootReducer = combineReducers({
-  posts: postsReducer
+  posts: postsReducer,
+  users: usersReducer,
 });
 
 export default configureStore({
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,9 +1,28 @@
-const { createSlice, nanoid } = require('@reduxjs/toolkit')
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { sub } from 'date-fns';
 
 const initialState = {
   posts: [
-    { id: '1', title: 'First Post!', content: 'Hello!' },
-    { id: '2', title: 'Second Post', content: 'More text' },
+    {
+      id: '1', title: 'First Post!', content: 'Hello!', date: sub(new Date(), { minutes: 10 }).toISOString(),
+      reactions: {
+        thumbsUp: 0,
+        hooray: 0,
+        heart: 0,
+        rocket: 0,
+        eyes: 0,
+      }
+    },
+    {
+      id: '2', title: 'Second Post', content: 'More text', date: sub(new Date(), { minutes: 5 }).toISOString(),
+      reactions: {
+        thumbsUp: 0,
+        hooray: 0,
+        heart: 0,
+        rocket: 0,
+        eyes: 0,
+      }
+    },
   ],
   status: 'idle',
   error: null,
@@ -17,20 +36,45 @@ const postsSlice = createSlice({
       reducer: (state, action) => {
         state.posts.push(action.payload)
       },
-      prepare: (title, content) => {
+      prepare: (title, content, userId) => {
         return {
           payload: {
             id: nanoid(),
+            date: new Date().toISOString(),
             title,
             content,
+            user: userId,
+            reactions: {
+              thumbsUp: 0,
+              hooray: 0,
+              heart: 0,
+              rocket: 0,
+              eyes: 0,
+            }
           },
         }
       },
     },
+    postUpdated(state, action) {
+      const { id, title, content } = action.payload;
+      const existingPost = state.posts.find(post => post.id === id);
+      if (existingPost) {
+        existingPost.title = title;
+        existingPost.content = content;
+      }
+    },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload
+      const existingPost = state.posts.find(post => post.id === postId)
+      if (existingPost) {
+        existingPost.reactions[reaction]++
+      }
+    }
   },
 })
 
-export const { postAdded } = postsSlice.actions
+export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
 export default postsSlice.reducer
 
 export const selectAllPosts = (state) => state.posts.posts
+export const selectPostById = (state, postId) => state.posts.posts.find(post => post.id === postId)
